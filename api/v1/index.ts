946bf104391d7a5b93fa85f890ebc942b5ca5093
Konrad (PC) <konrad@guzek.uk>
2024-12-17 00:55:12 +0100

    revert bad refactors, update to latest guzek-uk-commons

--- api/v1/index.ts
+++ api/v1/index.ts
@@ -1,19 +1,9 @@
 // Initialise dependencies
-import cors from "cors";
 import express from "express";
 import expressWs from "express-ws";
-import path from "path";
-import dotenv from "dotenv";
-const DEBUG_MODE = process.env.NODE_ENV === "development";
-// In production mode, this code is run from /api/v1/dist/index.js
-// In development mode, it is in /api/v1/index.ts
-// The env file is in /api/.env, so adjust accordingly
-const ENV_FILE_PATH = DEBUG_MODE ? "../.env" : "../../.env";
-const dotEnvPath = path.resolve(__dirname, ENV_FILE_PATH);
-dotenv.config({ path: dotEnvPath });
-const password = require("s-salt-pepper");
-
-import { sendError } from "guzek-uk-common/util";
+import { setupEnvironment } from "guzek-uk-common/setup";
+setupEnvironment();
+import { startServer } from "guzek-uk-common/util";
 import { getMiddleware } from "guzek-uk-common/middleware";
 import { getLogger } from "guzek-uk-common/logger";
 
@@ -30,7 +20,6 @@ const PORT = process.env.NODE_PORT;
 // Define the endpoints
 const ENDPOINTS = [
   "pages",
-  "auth",
   "updated",
   "tu-lalem",
   "liveseries",
@@ -38,24 +27,9 @@ const ENDPOINTS = [
   "torrents",
 ];
 
-if (DEBUG_MODE) {
-  // Add newline before app output for readability
-  console.log();
-}
-
 /** Initialises the HTTP RESTful API server. */
 async function initialise() {
-  const iterations = process.env.HASH_ITERATIONS;
-  if (!iterations) {
-    logger.error("No HASH_ITERATIONS environment variable set.");
-    return;
-  }
-  password.iterations(parseInt(iterations));
-  password.pepper(process.env.HASH_PEPPER);
-
   // Enable middleware
-  app.use(cors());
-  app.use(express.json());
   app.use(getMiddleware());
 
   // Enable individual API routes
@@ -64,15 +38,7 @@ async function initialise() {
     if (middleware.init) middleware.init(ENDPOINTS);
     app.use("/" + endpoint, middleware.router);
   }
-
-  // Catch-all 404 response for any other route
-  app.all("*", (req, res) =>
-    sendError(res, 404, {
-      message: `The resource at '${req.originalUrl}' was not located.`,
-    })
-  );
-
-  app.listen(PORT, () => logger.info(`API listening on port ${PORT}.`));
+  startServer(app, PORT);
 }
 
 if (PORT) {
