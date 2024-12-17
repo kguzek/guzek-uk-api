70ad5f9e9d331c0e400338953575549f8691ff8f
Konrad (PC) <konrad@guzek.uk>
2024-12-17 03:29:39 +0100

    update script

--- api/scripts/create_user.sql
+++ api/scripts/create_user.sql
@@ -1,12 +1,10 @@
-/**
- * Feel free to change the username as well as the password given to it
- */
+-- Feel free to change the username as well as the password given to it
 CREATE USER 'guzek_uk_api' @'localhost' IDENTIFIED BY 'CHANGE_THIS_PASSWORD';
-/* In an ideal world, we should strictly provide the grants the API has access
- * to a specific table rather than all the privileges.
- */
+-- In an ideal world, we should strictly provide the grants the API has access
+-- to a specific table rather than all the privileges.
 -- On some machines the underscore in `guzek_uk` needs to be escaped with a backslash,
 -- otherwise the user will not be able to access the database
 -- https://stackoverflow.com/a/44917370/15757366
-GRANT ALL PRIVILEGES ON `guzek\_uk`.`*` TO 'guzek_uk_api' @'localhost';
+-- GRANT ALL PRIVILEGES ON `guzek\_uk`.`*` TO 'guzek_uk_api' @'localhost';
+GRANT ALL PRIVILEGES ON guzek_uk.* TO 'guzek_uk_api' @'localhost';
 FLUSH PRIVILEGES;
